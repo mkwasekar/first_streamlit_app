f8e4d6c7d3817ed4e05d3d417ce260b7905b85a1
mkwasekar <110533679+mkwasekar@users.noreply.github.com>
2022-08-03 18:46:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text('🍚Boiled Eggs')
 
 streamlit.header('🍾Buit your own Breakfast Menu')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
