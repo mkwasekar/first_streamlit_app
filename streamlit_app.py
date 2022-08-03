d5da515c3cd3a3f00837d2cbcefd97b86a145a72
mkwasekar <110533679+mkwasekar@users.noreply.github.com>
2022-08-03 18:33:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents new Healthy dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and some fruits')
-streamlit.text('Boiled Eggs')
+streamlit.header('🌭🌭🌭Breakfast Menu')
+streamlit.text('🍍🍎🍓Omega 3 and some fruits')
+streamlit.text('🍚Boiled Eggs')
 
