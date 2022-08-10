70939917ae58870fe236f849c4a93084ae76f485
mkwasekar <110533679+mkwasekar@users.noreply.github.com>
2022-08-10 13:17:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,9 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
 my_data_row=my_cur.fetchone()
 streamlit.header("The fruit load list contains:"
 streamlit.dataframe(my_data_row)
+my_cur.execute("insert into fruit_load_list values ('from sreamlit') ")
+                 
